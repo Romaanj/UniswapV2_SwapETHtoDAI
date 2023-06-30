c20676dbd522dcb7b804c84b656ba69ee1b3f2cd
Steven <jwlee1218@g.skku.edu>
2023-06-30 17:28:18 +0900

    Changing hardhat network to Goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const {ALCHEMY_API_KEY,GOERLI_PRIVATE_KEY} = require("./private.json");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    hardhat: {
-      forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/{YOUR_API_KEY}",
-      }
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
     }
   }
 };
